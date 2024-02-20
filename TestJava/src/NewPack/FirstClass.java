5f4eb6135057eaf72b7c1a375741bb7d743a197e
rasel <rasel@DESKTOP-IIGTNJ0.mynetworksettings.com>
2024-02-20 15:57:46 -0500

    first code

--- TestJava/src/NewPack/FirstClass.java
+++ TestJava/src/NewPack/FirstClass.java
@@ -4,6 +4,9 @@ public class FirstClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//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 (Password for gitpush.)
+		
+		System.out.println("First Java Project");
 
 	}
 
