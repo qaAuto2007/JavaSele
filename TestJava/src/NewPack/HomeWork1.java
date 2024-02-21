8bb9e22606028b5fc279a020c5de3627120f3337
rasel <rasel@DESKTOP-IIGTNJ0.mynetworksettings.com>
2024-02-21 12:15:08 -0500

    Home work 1

---
+++ TestJava/src/NewPack/HomeWork1.java
@@ -0,0 +1,48 @@
+package NewPack;
+
+public class HomeWork1 {
+	public static void main(String[] args){
+		/**
+		 Store below scenario value in an appropriate datatype variable, and print the same in console.
+		 1.number of hours in a day
+		 2.number of max days in a year
+		 3.total number of employees in an organization
+		 4.population of a country
+		 5.interest rate
+		 6.balance in a bank account
+		 7.does sun rise from the west? store answer
+		 8.initials of your name
+		 9.your name
+		 */
+		
+		byte numberOfHoursInADay=24;
+		System.out.println("Total number of hours in a day -> "+numberOfHoursInADay);
+		
+		short numberOfMaximumDaysInAYear=366;
+		System.out.println("Total number of maximum days in a year -> "+numberOfMaximumDaysInAYear);
+		
+		int numberOfEmployees= 1608000;
+		System.out.println("Total number of employees for Amazon -> "+numberOfEmployees);
+		
+		long populationOfIndia = 1408000000L;
+		System.out.println("Population of India -> "+populationOfIndia);
+		
+		float currentMortgageInterestRate = 5.25f;
+		System.out.println("Current mortgage rate -> "+currentMortgageInterestRate);
+		
+		double bankBalance=23457.48;
+		System.out.println("Current bank balance -> "+bankBalance);
+		
+		boolean doesSunRiseFromWest=false;
+		System.out.println("Does sun rise from the west? -> "+doesSunRiseFromWest);
+		
+		char initialOfMyFirstName='R';
+		System.out.println("My first name initial is -> "+initialOfMyFirstName);
+		
+		String fullName="Rasel Chowdhury";
+		System.out.println("My name is -> "+fullName);
+		
+		
+
+}
+}
