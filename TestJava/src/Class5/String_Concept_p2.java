f596c0522c84841e5785310e5cf28090a9d983de
rasel <rasel@DESKTOP-IIGTNJ0.mynetworksettings.com>
2024-02-25 20:06:05 -0500

    string

--- TestJava/src/Class5/String_Concept_p2.java
+++ TestJava/src/Class5/String_Concept_p2.java
@@ -1,5 +1,7 @@
 package Class5;
 
+import java.util.Arrays;
+
 public class String_Concept_p2 {
 
 	public static void main(String[] args) {
@@ -26,7 +28,7 @@ public class String_Concept_p2 {
 		 * 
 		 * Input beginIndex and endIndex
 		 * 			if beginIndex and endIndex are valid
-		 * 			returns string from befinIndex to endIndex-1
+		 * 			returns string from beginIndex to endIndex-1
 		 * 			else string index out of bound
 		*/
 		cityName="New York City In The ToWN of USA";
@@ -37,6 +39,26 @@ public class String_Concept_p2 {
 		System.out.println("Substring from index 10 to 15: "+subFrom10to15);//Substring from index 10 to 15: ity I (10 to 14)
 																			//Input beginIndex and endIndex
 		 																	//returns string from befinIndex to endIndex-1
+		
+		/*
+		 * to cut the String value from a specific point to multiple Strings
+		 * Method: split()
+		 */
+		//String cityName="New York City In The ToWN of USA";
+		
+		cityName="New York City In The ToWN of USA";
+		String [] afterSplitBy_o=cityName.split("o"); 
+		//shortcut to print array System.out.println(Arrays.toString(afterSplitBy_o)); 
+		System.out.println("Array after split from 'o': "+Arrays.toString(afterSplitBy_o)); //[New Y, rk City In The T, WN , f USA]
+		
+		String[]afterSplitEachCharacter=cityName.split(""); //Array with every character at each index
+		System.out.println("Array after split by each character: "+Arrays.toString(afterSplitEachCharacter));
+		//[N, e, w,  , Y, o, r, k,  , C, i, t, y,  , I, n,  , T, h, e,  , T, o, W, N,  , o, f,  , U, S, A]
+		
+		String[]afterSplitBySpace=cityName.split(" "); // split by space
+		System.out.println("Array after split by each character: "+Arrays.toString(afterSplitBySpace));
+		//[New, York, City, In, The, ToWN, of, USA]
+		
 	}
 
 }
